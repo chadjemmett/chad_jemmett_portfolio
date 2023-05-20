776b70d00c51a318c13c1df75e5d127fd8adf3b6
chadjemmett <chad.jemmett@gmail.com>
2023-05-20 13:33:37 -0500

    Adding leaderboard app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from game_data import DATA
 import os
-
 
 app = Flask(__name__)
 
-
+msg = [{'title': "message one"}]
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
@@ -12,6 +12,13 @@ def home():
 @app.route('/template', methods=['GET'])
 def template():
     return render_template('template.html')
+
+
+@app.route('/leaderboard', methods=['GET', 'POST'])
+def leaderboard():
+    print('values', request.values)
+    return render_template('leaderboard.html', data=DATA)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
